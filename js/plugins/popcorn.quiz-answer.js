a0340dfb922c1c70081ad94dde7a3bf545bb0abd
Brian Chirls <brian@chirls.com>
2012-04-04 14:52:10 -0400

    Answers plugin highlights multiple choice answers as they are read.

---
+++ js/plugins/popcorn.quiz-answer.js
@@ -0,0 +1,62 @@
+(function (Popcorn) {
+
+"use strict";
+
+	var styleSheet;
+
+	Popcorn.basePlugin( 'answer' , function(options, base) {
+		var popcorn = this,
+			media = popcorn.media,
+			selector = '', elements;
+
+		if (!options.target) {
+			return;
+		}
+
+		if (options.question === undefined || isNaN(options.question) || options.question < 0) {
+			return;
+		}
+
+		if (!styleSheet) {
+			styleSheet = document.createElement('style');
+			styleSheet.setAttribute('type', 'text/css');
+			styleSheet.appendChild(
+				document.createTextNode(
+					'.popcorn-quiz-answer.highlight { background-color: yellow; }\n'
+				)
+			);
+			document.head.appendChild(styleSheet);
+		}
+
+		if (options.target.id) {
+			selector = '#' + options.target.id + ' ';
+		}
+
+		selector += '.popcorn-quiz.active .answer-' + options.question;
+
+		return {
+			start: function( event, options ) {
+				var i;
+				elements = document.querySelectorAll(selector);
+				for (i = 0; i < elements.length; i++) {
+					base.addClass(elements[i], 'highlight');
+				}
+			},
+			frame: function( event, options, time ) {
+			},
+			end: function( event, options ) {
+				var i;
+
+				if (!elements) {
+					return;
+				}
+
+				for (i = 0; i < elements.length; i++) {
+					base.removeClass(elements[i], 'highlight');
+				}
+			},
+			_teardown: function( options ) {
+			}
+		};
+	});
+})( Popcorn );
