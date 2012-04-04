bc750c648b0b452191d92a8b598cd188fc6e11b4
Brian Chirls <brian@chirls.com>
2012-04-04 17:17:04 -0400

    quiz questions can now be answered. explanation displayed

--- js/plugins/popcorn.quiz.js
+++ js/plugins/popcorn.quiz.js
@@ -1,14 +1,52 @@
-(function (Popcorn) {
+(function (Popcorn, window) {
 
 "use strict";
 
-	var styleSheet;
+	var styleSheet,
+		console = window.console;
 
 	Popcorn.basePlugin( 'quiz' , function(options, base) {
 		var popcorn = this,
 			media = popcorn.media,
 			guid,
-			i, ul, li, answer, answers;
+			i, ul, li, answer, answers,
+			question,
+			explanation;
+
+		function clickAnswer(i) {
+			if (answer >= 0) {
+				//don't re-answer this until reset
+				return;
+			}
+
+			answer = i;
+			options.answer = i;
+
+			base.addClass(answers[i].label.parentNode, 'answered');
+			if (base.options.correct === i) {
+				base.addClass(base.container, 'right');
+				options.correct = true;
+			} else {
+				base.addClass(base.container, 'wrong');
+				options.correct = false;
+			}
+
+			if (typeof options.onAnswer === 'function') {
+				if (Popcorn.plugin.debug) {
+					options.onAnswer(options);
+				} else {
+					try {
+						options.onAnswer(options);
+					} catch (e) {
+						console.log('Error in quiz onAnswer event:' + e.message);
+					}
+				}
+			}
+		}
+
+		function proceed() {
+			popcorn.play();
+		}
 
 		if (!options.question || !options.target || !options.answers) {
 			return;
@@ -28,9 +66,14 @@
 			styleSheet.appendChild(
 				document.createTextNode(
 					'.popcorn-quiz { display: none; }\n' +
+					'.popcorn-quiz > .popcorn-quiz-explanation { display: none; }\n' +
+					'.popcorn-quiz.right > .popcorn-quiz-explanation, .popcorn-quiz.wrong > .popcorn-quiz-explanation { display: block; }\n' +
 					'.popcorn-quiz > ul { list-style: none; }\n' +
 					'.popcorn-quiz-answer { cursor: pointer; }\n' +
 					'.popcorn-quiz-answer > label { cursor: pointer; }\n' +
+					'.popcorn-quiz.wrong .popcorn-quiz-answer, .popcorn-quiz.right .popcorn-quiz-answer { color: gray; }\n' +
+					'.popcorn-quiz.wrong .popcorn-quiz-answer.answered { text-decoration: line-through; }\n' +
+					'.popcorn-quiz.wrong .popcorn-quiz-answer.correct, .popcorn-quiz.right .popcorn-quiz-answer.correct { font-weight: bold; color: darkgreen; }\n' +
 					'.popcorn-quiz.active { display: block; }\n'
 			));
 			document.head.appendChild(styleSheet);
@@ -38,8 +81,10 @@
 
 		base.makeContainer();
 
-		//todo: put this in an element for styling
-		base.container.appendChild(document.createTextNode(options.question));
+		question = document.createElement('p');
+		base.addClass(question, 'popcorn-quiz-question');
+		question.appendChild(document.createTextNode(options.question));
+		base.container.appendChild(question);
 
 		ul = document.createElement('ul');
 		base.container.appendChild(ul);
@@ -57,15 +102,34 @@
 			answer.label.appendChild(answer.input);
 			answer.label.appendChild(document.createTextNode(answer.text));
 
+			answer.label.addEventListener('click', (function(i) {
+				return function() {
+					clickAnswer(i);
+				};
+			}(i)), false);
+
 			li = document.createElement('li');
 			li.appendChild(answer.label);
 			base.addClass(li, ['answer-' + i, 'popcorn-quiz-answer']);
 
 			ul.appendChild(li);
+
+			if (i === options.correct) {
+				base.addClass(li, 'correct');
+			}
+		}
+		answer = -1;
+
+		if (options.explanation) {
+			explanation = document.createElement('div');
+			base.addClass(explanation, 'popcorn-quiz-explanation');
+			explanation.innerHTML = options.explanation;
+			base.container.appendChild(explanation);
 		}
 
 		return {
 			start: function( event, options ) {
+				base.removeClass(base.container, ['right','wrong']);
 				base.addClass(base.container, 'active');
 			},
 			frame: function( event, options, time ) {
@@ -80,4 +144,4 @@
 			}
 		};
 	});
-})( Popcorn );
+})( Popcorn, window );
